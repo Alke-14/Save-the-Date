5c58f9b68b0c29958def5636fa604908c24a4e53
Kevin Hernandez <kevinalbertohernandezgarcia@gmail.com>
2026-08-05 16:45:52 -0500

    okay this should help frfr

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Save-the-Date/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
